b8f19ffcff892b9c5ea4117ea7786d6c8956f7f8
Daniel Richardson <daniel.richardson@uea.ac.uk>
2023-09-08 19:58:14 +0100

    Functions and logic for calculating piw, piT, and dXY. Now includes monoallelic sites above threshold depth.

--- treeXY.py
+++ treeXY.py
@@ -1,5 +1,7 @@
-import sys
+import random
+import itertools
 from argparse import ArgumentParser
+# import math as maths
 
 ##########################
 # command line arguments #
@@ -41,6 +43,35 @@ args = parser.parse_args()
 #############
 # functions #
 #############
+# def initialise_windows():
+#     # windows
+#     # check the total number of windows
+#     with open(args.file) as f:
+#         for line in f:
+#             # strip trailing newline
+#             line = line.strip("\n")
+#             line = line.split("\t")
+#             # genomic position
+#             pos = line[1]
+#         max_pos = int(pos)
+#
+#     window_size = int(args.window_size)
+#     window_slide = int(args.window_slide)
+#
+#     # initialise window_dict of correct length
+#     tot_windows = maths.floor(max_pos / window_slide)
+#
+#     # initialise list of all coords, including non-biallelic
+#     # all_coords = [n for n in range(1, max_pos)]
+#
+#     window_dict = {}
+#     for i in range(0, tot_windows):
+#         start_coord = i * window_slide
+#         end_coord = start_coord + window_size + 1
+#         if end_coord > max_pos:
+#             end_coord = max_pos + 1
+#         window_dict[range(start_coord, end_coord)] = [[], [], []]
+
 def get_sync_counts(sync_site_counts):
     # split all count columns and record counts as integers
     # order in SYNC files is A, T, C, G, N, del
@@ -101,7 +132,7 @@ def check_allele_num(sync_count_list, dpth_pass_list):
         return []
 
 
-def check_pop_alleles(sync_count_list, dpth_pass_list, called_alleles):
+def check_pop_alleles(sync_count_list, dpth_pass_list, called_alleles, expected_alleles):
     # check depth of alleles in individual pops
     # **** THIS IS REALLY BAD
 
@@ -118,7 +149,7 @@ def check_pop_alleles(sync_count_list, dpth_pass_list, called_alleles):
         for i in ad_list:
             ad_len_list.append(len(i))
 
-        if ad_len_list.count(2) > 1:
+        if ad_len_list.count(expected_alleles) >= args.min_allele_pops:
             return True
         else:
             return False
@@ -149,6 +180,55 @@ def filter_triallelic(sync_count_list):
     return edited_list
 
 
+def get_allele_freqs(allele_inds, counts, names):
+    pop_dict = {}
+    # randomise which allele is which
+    # conventionally, p is often designated as the major allele
+    # however, we tend to assign it randomly
+    # **** specify from command line whether p = major allele or random allele 1
+    random.shuffle(allele_inds)
+    if len(allele_inds) == 2:
+        # what is the index of p and q in the list of alleles?
+        p_ind = allele_inds[0]
+        q_ind = allele_inds[1]
+        # calculate p and q for each population
+        # only p is used in calculating dXY
+        for ecounts in enumerate(counts):
+            count_ind = ecounts[0]
+            counts = ecounts[1]
+            curr_pop = names[count_ind]
+            p_count = counts[p_ind]
+            q_count = counts[q_ind]
+            if p_count == 0 & q_count == 0:
+                p = 0
+                q = 0
+                pop_dict[curr_pop] = [p, q]
+            else:
+                p = p_count / (p_count + q_count)
+                q = q_count / (p_count + q_count)
+                pop_dict[curr_pop] = [p, q]
+
+    if len(allele_inds) == 1:
+        # what is the index of p in the list of alleles?
+        p_ind = allele_inds[0]
+        # calculate p and q for each population
+        for ecounts in enumerate(counts):
+            count_ind = ecounts[0]
+            counts = ecounts[1]
+            curr_pop = names[count_ind]
+            p_count = counts[p_ind]
+            if p_count == 0:
+                p = 0
+                q = 0
+                pop_dict[curr_pop] = [p, q]
+            else:
+                p = 1
+                q = 0
+                pop_dict[curr_pop] = [p, q]
+
+    return pop_dict
+
+
 # calculate Nei's dXY between two populations
 # corresponds to raw dXY in David's SlidingWindows program
 def get_dxy(p1, p2):
@@ -163,9 +243,29 @@ def get_piw(p1):
     return piw
 
 
-########
-# main #
-########
+# calculate piT for a given population pair
+def get_pit(p1, p2, q1, q2):
+    p_bar = (p1 + p2) / 2
+    q_bar = (q1 + q2) / 2
+    pit = 2 * p_bar * q_bar
+    return pit
+
+
+def get_site_stats():
+    pass
+
+
+def get_site_trees():
+    pass
+
+
+def get_site_tree_stats():
+    pass
+
+
+##########
+# treeXY #
+##########
 with open(args.file) as f:
     for line in f:
         # strip trailing newline
@@ -180,6 +280,8 @@ with open(args.file) as f:
         # all colon delimited count columns
         site_counts = line[3:]
         count_list = get_sync_counts(site_counts)
+        # **** eventually, want to read names from file
+        pop_names = [str(i) for i in range(1, len(count_list) + 1)]
 
         # read depth checks
         dpth_dict = {}
@@ -201,9 +303,83 @@ with open(args.file) as f:
             # get alleles
             alleles = check_allele_num(count_list, pop_dpth)
 
-        # if biallelic, proceed with calculations
-        # **** before proceeding, need to deal with min_allele_pops
+        # dict to store all stats for each position
+        pos_stats_dict = {}
+
+        # if biallelic, proceed with piT / dXY / tree calculations
         # **** it is only here that I start to ignore lines below read depth threshold, does that make sense?
+        # **** I could provide option to output filtered SYNC file here
         if len(alleles) == 2:
-            if check_pop_alleles(count_list, pop_dpth, alleles):
-                print(line)
+            if check_pop_alleles(count_list, pop_dpth, alleles, 2):
+                # initialise dict key
+                pos_stats_dict[pos] = []
+                # calculate p and q for all pops
+                freqs_dict = get_allele_freqs(alleles, count_list, pop_names)
+                # make list of valid pops based on indices of pop_dpth
+                dpth_pass_pops = [i for i, e in enumerate(pop_dpth) if e == 1]
+                # calculate piw for valid pops
+                pop_piw_dict = {}
+                for pop in dpth_pass_pops:
+                    pop = pop_names[pop]
+                    pop_pq = freqs_dict[pop]
+                    pop_piw = get_piw(pop_pq[0])
+                    pop_piw_dict["piw_" + pop] = pop_piw
+
+                pos_stats_dict[pos].append(pop_piw_dict)
+
+                # make list of valid comparisons based on indices of pop_dpth
+                dpth_pass_comps = itertools.combinations(dpth_pass_pops, 2)
+                # calculate stats for valid pops / comps
+                pop_pit_dict = {}
+                pop_dxy_dict = {}
+                for comp in dpth_pass_comps:
+                    pop1 = pop_names[comp[0]]
+                    pop2 = pop_names[comp[1]]
+                    pop1_pq = freqs_dict[pop1]
+                    pop2_pq = freqs_dict[pop2]
+                    pops_pit = get_pit(pop1_pq[0], pop2_pq[0], pop1_pq[1], pop2_pq[1])
+                    pops_dxy = get_dxy(pop1_pq[0], pop2_pq[0])
+                    pop_pit_dict["piT_" + pop] = pops_pit
+                    pop_dxy_dict["dXY_" + pop] = pops_dxy
+
+                pos_stats_dict[pos].append(pop_pit_dict)
+                pos_stats_dict[pos].append(pop_dxy_dict)
+
+        # if monoallelic, calculate piw
+        # **** piT and dXY will always evaluate to zero, but need to include in output anyway
+        if len(alleles) == 1:
+            if check_pop_alleles(count_list, pop_dpth, alleles, 1):
+                # initialise dict key
+                pos_stats_dict[pos] = []
+                # calculate p and q for all pops
+                freqs_dict = get_allele_freqs(alleles, count_list, pop_names)
+                # make list of valid pops based on indices of pop_dpth
+                dpth_pass_pops = [i for i, e in enumerate(pop_dpth) if e == 1]
+                # calculate piw for valid pops
+                pop_piw_dict = {}
+                for pop in dpth_pass_pops:
+                    pop = pop_names[pop]
+                    pop_pq = freqs_dict[pop]
+                    pop_piw = get_piw(pop_pq[0])
+                    pop_piw_dict["piw_" + pop] = pop_piw
+
+                # make list of valid comparisons based on indices of pop_dpth
+                dpth_pass_comps = itertools.combinations(dpth_pass_pops, 2)
+                # calculate stats for valid pops / comps
+                pop_pit_dict = {}
+                pop_dxy_dict = {}
+                for comp in dpth_pass_comps:
+                    pop1 = pop_names[comp[0]]
+                    pop2 = pop_names[comp[1]]
+                    pop_1_2 = pop1 + "_" + pop2
+                    pop1_pq = freqs_dict[pop1]
+                    pop2_pq = freqs_dict[pop2]
+                    pops_pit = get_pit(pop1_pq[0], pop2_pq[0], pop1_pq[1], pop2_pq[1])
+                    pops_dxy = get_dxy(pop1_pq[0], pop2_pq[0])
+                    pop_pit_dict["piT_" + pop_1_2] = pops_pit
+                    pop_dxy_dict["dXY_" + pop_1_2] = pops_dxy
+
+                # compile stats
+                pos_stats_dict[pos].append(pop_piw_dict)
+                pos_stats_dict[pos].append(pop_pit_dict)
+                pos_stats_dict[pos].append(pop_dxy_dict)
